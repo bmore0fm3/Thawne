bb8f38f771566569066b9fed9030c40ab6ecd7dd
Bmore0fm3 <36344386+bmore0fm3@users.noreply.github.com>
2023-05-20 14:56:44 -0400

    Completed pages
    Updated code and pages

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Thawne
+![alt text](img/Thawne.gif "Thawne Logo")
 Thawne. A javascript exercise using arrays. Write a program that displays a string in reverse.
--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+//Get the string from the page
+//controller function
+function getValues(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let reversedString = reverseString(userString);
+
+    displayString(reversedString);
+
+}
+
+
+//Reverse the string
+//logic function
+function reverseString(userString){
+    let revString = [];
+
+    for (let index = userString.length - 1; index >= 0; index --){
+        revString += userString[index];
+    }
+
+    return revString
+}
+
+//Display reversed string to the user
+//display function
+function displayString(reversedString){
+
+    //write message to page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${reversedString}`;
+
+    //show alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
